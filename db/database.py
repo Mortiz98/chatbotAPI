cccba13c726740e77027268e00f04774db046a00
Mateo <mortiz9806@gmail.com>
2025-07-17 17:46:59 -0500

    update

--- db/database.py
+++ db/database.py
@@ -1,4 +1,3 @@
-from multiprocessing.connection import Client
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker, Session
